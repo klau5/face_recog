26a2fa8325189b6d5f919d721a37b7753b2ba0f0
klau5_dev <jografie@gmail.com>
2019-04-30 13:45:28 +0530

    front

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'klau5, this is search request...'}
+            </div>
+            <div className='white f1'>
+                {'#14'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
